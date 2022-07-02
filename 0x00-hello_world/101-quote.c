1cd6c2c75c200c774c76c26586d5a3f28eed5aff
Sefineh1 <sefinehad@gmail.com>
2022-07-01 22:50:43 -0700

    intel

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *  main: Entery point
+ *  Return: 1 standard error
+ */
+int main(void)
+
+{
+
+	fprintf("and that piece of art is useful" " - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+
+}
