b6a5673c4f0498b49ca0e8f76940ead6fcefe21b
Sefineh1 <sefinehad@gmail.com>
2022-07-01 22:20:55 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
-/** main-  Entry point
+/**
+ * main-  Entry point
  * Return: the size of different types of variable
  */
 int main(void)
-{ 
+
+{
+
 	int a;
 	long int b;
 	long long int c;
 	char d;
 	float f;
-	printf("size of a char: %1u byte(s)\n", (unsigned)sizeof(d));
-	printf("size of an int: %1u byte(s)\n", (unsigned)sizeof(a));
-	printf("size of a long int: %1u byte(s)\n", (unsigned)sizeof(b));
-	printf("size of a long long int: %1u byte(s)\n", (unsigned)sizeof(c));
-	printf("size of a float: %1u byte(s)\n", (unsigned)sizeof(f));
-	return(0);
+
+	printf("size of a char: %1u byte(s)\n", (unsigned int)sizeof(d));
+	printf("size of an int: %1u byte(s)\n", (unsigned int)sizeof(a));
+	printf("size of a long int: %1u byte(s)\n", (unsigned int)sizeof(b));
+	printf("size of a long long int: %1u byte(s)\n", (unsigned int)sizeof(c));
+	printf("size of a float: %1u byte(s)\n", (unsigned int)sizeof(f));
+	return (0);
+
 }
