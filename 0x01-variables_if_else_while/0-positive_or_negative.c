feb2876a58d0247b1aa1234fb957a64e6e17b0f4
Sefineh1 <sefinehad@gmail.com>
2022-07-01 19:53:17 -0700

    positive and negative integer identifier

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/** main: function will assign a random number n each time* it is exceuted, it prints a postive negative of the numb*er stored in n */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+if (n>0)
+	printf("%d is positive\n", n);
+if (n==0)
+	printf("%d is zero\n", n);
+if (n<0)
+	printf("%d is negative\n", n)
+	return (0);
+}
