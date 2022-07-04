72318cdcf687ee98b8ac5518077725fcf83d6abc
Sefineh1 <sefinehad@gmail.com>
2022-07-04 15:43:28 -0700

    c-programm

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always return 0
+ *
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 'z'; a >= 'a'; a++)
+	putchar(a);
+	putchar('\n');
+
+	return (0);
+}
